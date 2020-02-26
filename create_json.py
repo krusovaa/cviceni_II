9533105d97682c9a29d2d52562dda4a53d81c9dc
Anna Krusova <anicka.krusova@gmail.com>
2020-02-26 12:12:56 +0100

    posunuti to_json o uroven vys

--- create_json.py
+++ create_json.py
@@ -1,18 +1,31 @@
 import json
 
+
 class Object(object):
-    def __init__(self,type):
+    def __init__(self, type):
         self.type = type
 
+    def to_json(self):
+        return {"type": self.type}
+
+
 class Geometry(Object):
-    def __init__(self,type,coordinates):
+    def __init__(self, type, coordinates):
         super().__init__(type)
         self.coordinates = coordinates
+
+    def to_json(self):
+        dic = super().to_json()
+        dic["coordinates"] = self.coordinates
+        return dic
+        # premisteno sem z point in linestring, protoze oboje dedi z geometry, tak to muzu presunout vys
+        # malokdy vyse nez o jednoho predka
+
 
 class Feature(Object):
     def __init__(self, geometry, properties, id=None):
         super().__init__("Feature")
-        if not isinstance(geometry,Geometry):
+        if not isinstance(geometry, Geometry):
             raise ValueError("Attempted to insert non Geometry as a geometry")
         self.geometry = geometry
         if id:
@@ -20,39 +33,46 @@ class Feature(Object):
         self.properties = properties
 
     def to_json(self):
-        return {"type": "Feature", "geometry": self.geometry.to_json(),
-                    "properties": self.properties}
+        dic = super().to_json()
+        dic["geometry"] = self.geometry.to_json()  # objekt typu geometry
+        dic["properties"] = self.properties  # properties je slovnik
+        return dic
+        # return {"type": "Feature", "geometry": self.geometry.to_json(),     "properties": self.properties}
 
 
 class FeatureCollection(Object):
-    def __init__(self,features):
+    def __init__(self, features):
         super().__init__("FeatureCollection")
         self.features = features
 
     def to_json(self):
+        dic = super().to_json()
         features = [f.to_json() for f in self.features]
-        return {"type": "FeatureCollection", "features": features}
+        dic["features"] = features
+        return dic
+        # return {"type": "FeatureCollection", "features": features}
+
 
 class Point(Geometry):
-    def __init__(self,x,y,z=None):
+    def __init__(self, x, y, z=None):
         if z is not None:
-            super().__init__("Point",[x,y,z])
+            super().__init__("Point", [x, y, z])
         else:
-            super().__init__("Point",[x,y])
-
-    def to_json(self):
-        return {"type": "Point", "coordinates": self.coordinates}
+            super().__init__("Point", [x, ])
 
 
 class LineString(Geometry):
-    def __init__(self,coords):
-        super().__init__("LineString",coords)
+    def __init__(self, coords):
+        super().__init__("LineString", coords)
 
-    def to_json(self):
-        return {"type": "LineString", "coordinates": self.coordinates}
 
-p = Point(10,10)
+class Polygon(Geometry):
+    def __init__(self, coords):
+        super().__init__("polygon", coords)
+
+
+p = Point(10, 10)
 print(json.dumps(p.to_json()))
-f = Feature(p,{"test":"value"})
+f = Feature(p, {"test": "value"})
 print(json.dumps(f.to_json()))
 print(f.geometry.coordinates)
