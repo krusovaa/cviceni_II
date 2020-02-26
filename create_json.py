d46d0a7d54055c0ec1df70f82923080fe5df7876
Anna Krusova <anicka.krusova@gmail.com>
2020-02-26 13:08:14 +0100

    pridano from_json

--- create_json.py
+++ create_json.py
@@ -10,6 +10,16 @@ class Object(object):
 
 
 class Geometry(Object):
+    @classmethod
+    def from_json(cls, adict):
+        if adict["type"] == "LineString":
+            obj = LineString.from_json(adict)
+        elif adict["type"] == "Point":
+            obj = Point.from_json(adict)
+        elif adict["type"] == "Polygon":
+            obj = Polygon.from_json(adict)
+        return obj
+
     def __init__(self, type, coordinates):
         super().__init__(type)
         self.coordinates = coordinates
@@ -23,6 +33,13 @@ class Geometry(Object):
 
 
 class Feature(Object):
+    @classmethod
+    def from_json(cls, adict):
+        properties = adict["properties"]
+        geometry = Geometry.from_json(adict["geometry"])
+        obj = cls(geometry, properties)
+        return obj
+
     def __init__(self, geometry, properties, id=None):
         super().__init__("Feature")
         if not isinstance(geometry, Geometry):
@@ -41,6 +58,15 @@ class Feature(Object):
 
 
 class FeatureCollection(Object):
+    @classmethod
+    def from_json(cls, adict):  # cls zkratka nasi class (tady FeatureCollection)
+        feature_objs = []
+        for fdict in adict["features"]:
+            fobj = Feature.from_json(fdict)
+            feature_objs.append(fobj)
+        obj = cls(feature_objs)  # ekvivalentni FeatureColection(feature_objs)
+        return obj
+
     def __init__(self, features):
         super().__init__("FeatureCollection")
         self.features = features
@@ -54,25 +80,65 @@ class FeatureCollection(Object):
 
 
 class Point(Geometry):
+    @classmethod
+    def from_json(cls, adict):
+        coord_x = adict["coordinates"][0]
+        coord_y = adict["coordinates"][1]
+        obj = cls(coord_x, coord_y)
+        return obj
+
     def __init__(self, x, y, z=None):
         if z is not None:
             super().__init__("Point", [x, y, z])
         else:
-            super().__init__("Point", [x, ])
+            super().__init__("Point", [x, y])
 
 
 class LineString(Geometry):
+    @classmethod
+    def from_json(cls, adict):
+        coords = adict["coordinates"]
+        obj = cls(coords)
+        return obj
+
     def __init__(self, coords):
         super().__init__("LineString", coords)
 
 
 class Polygon(Geometry):
+    @classmethod
+    def from_json(cls, adict):
+        coords = adict["coordinates"]
+        obj = cls(coords)
+        return obj
+
     def __init__(self, coords):
         super().__init__("polygon", coords)
 
 
+def from_json(adict):
+    if adict["type"] == "FeatureCollection":
+        return FeatureCollection.from_json(adict)
+    elif adict["type"] == "Feature":
+        return Feature.from_json(adict)
+    elif adict["type"] == "Geometry":
+        return Point.from_json(adict)
+    else:
+        print("Unknown type {}".format(adict["type"]))
+
+    # zjisti co je to zac
+    # vytvori oibjektovou hierarchii
+    # vrati korenovy element (Feature / Feature Collection / Geometry)
+
+
+# FeatureCollection.from_json(None)
 p = Point(10, 10)
-print(json.dumps(p.to_json()))
+# print(json.dumps(p.to_json()))
 f = Feature(p, {"test": "value"})
-print(json.dumps(f.to_json()))
-print(f.geometry.coordinates)
+# print(json.dumps(f.to_json()))
+# print(f.geometry.coordinates)
+
+with open("BEZ_ObjektMPP_b.json", 'r', encoding='utf-8') as f:
+    adict = json.load(f)
+print(adict)
+print(from_json(adict).to_json())
